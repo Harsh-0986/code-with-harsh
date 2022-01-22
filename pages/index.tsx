16396181aa979c22e0c4e2d32b4f74498b0bfed4
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-01-22 15:21:52 +0530

    Implemented the posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 
-import { sanityClient } from "../sanity";
+import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
 
 interface Props {
@@ -9,9 +10,6 @@ interface Props {
 }
 
 export default function Home({ posts }: Props) {
-  //Getting the posts
-  console.log(posts);
-
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -45,6 +43,34 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden">
+              <img
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+              />
+              <div className="flex justify-between p-5 bg-white">
+                {/* Title and description */}
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                {/* Author's image */}
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
