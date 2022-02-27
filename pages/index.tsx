8d97de0b50a77a20fc1dded653eb4e129f9e638d
Harsh-0986 <79073866+Harsh-0986@users.noreply.github.com>
2022-02-27 11:39:16 +0530

    Updated tagline

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({ posts }: Props) {
 
       {/* Main Banner */}
       <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-        <div className="px-10 space-y-5">
+        <div className="p-10 space-y-5">
           {/* Tagline */}
           <h1 className="text-6xl max-w-xl font-serif">
             <span className="underline decoration-black decoration-4 my-12">
