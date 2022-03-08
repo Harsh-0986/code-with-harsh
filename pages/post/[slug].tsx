35b95416e71d2f5b46a525ea22e9a970d2753055
Harsh-0986 <79073866+Harsh-0986@users.noreply.github.com>
2022-03-08 10:10:36 +0530

    Update code blocks

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -29,22 +29,22 @@ const serializers = {
         </SyntaxHighlighter>
       );
     },
-  },
 
-  h1: (props: any) => <h1 className="text-4xl font-bold my-5" {...props} />,
-  h2: (props: any) => <h2 className="text-3xl font-bold my-5" {...props} />,
-  h3: (props: any) => {
-    return <h3 className="text-2xl font-bold my-5" {...props} />;
+    h1: (props: any) => <h1 className="text-4xl font-bold my-5" {...props} />,
+    h2: (props: any) => <h2 className="text-3xl font-bold my-5" {...props} />,
+    h3: (props: any) => {
+      return <h3 className="text-2xl font-bold my-5" {...props} />;
+    },
+    h4: (props: any) => {
+      return <h4 className="text-xl font-bold my-5" {...props} />;
+    },
+    li: ({ children }: any) => <li className="ml-4 list-disc">{children}</li>,
+    link: ({ href, children }: any) => (
+      <a href={href} className="text-blue-500 hover:underline">
+        {children}
+      </a>
+    ),
   },
-  h4: (props: any) => {
-    return <h4 className="text-xl font-bold my-5" {...props} />;
-  },
-  li: ({ children }: any) => <li className="ml-4 list-disc">{children}</li>,
-  link: ({ href, children }: any) => (
-    <a href={href} className="text-blue-500 hover:underline">
-      {children}
-    </a>
-  ),
 };
 
 interface Props {
