c1e59075d2b73040179e7e3698b2d84a0ba8dac0
Harsh-0986 <79073866+Harsh-0986@users.noreply.github.com>
2022-03-08 10:48:49 +0530

    Fixed h3 styles

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import BlockContent from "@sanity/block-content-to-react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import Head from "next/head";
-import '../../components/slug.css'
+import '../../components/Slug.module.css'
 
 interface IFormInput {
   _id: string;
